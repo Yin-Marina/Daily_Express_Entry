ac9b85c40754ef21b6531303b869c621efd48503
MutaoYin <72797020+Yin-Marina@users.noreply.github.com>
2024-05-12 18:53:13 -0400

    beautify the page

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,40 @@
 async function fetchData() {
     try {
-        const response = await fetch('https://www.canada.ca/content/dam/ircc/documents/json/ee_rounds_123_en.json#/rounds/data.json');
+        const response = await fetch('https://your-json-link.com/data.json');
         const data = await response.json();
         displayData(data);
     } catch (error) {
-        document.getElementById('data-container').innerHTML = 'Error loading data.';
+        document.getElementById('data-container').innerHTML = '<div class="alert alert-danger" role="alert">Error loading data.</div>';
         console.error('Error fetching data:', error);
     }
 }
 
 function displayData(data) {
     const container = document.getElementById('data-container');
-    container.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+    if (Array.isArray(data)) {
+        container.innerHTML = createTable(data);
+    } else {
+        container.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+    }
+}
+
+function createTable(data) {
+    let table = '<table class="table table-striped">';
+    table += '<thead class="thead-dark"><tr>';
+    Object.keys(data[0]).forEach(key => {
+        table += `<th scope="col">${key}</th>`;
+    });
+    table += '</tr></thead>';
+    table += '<tbody>';
+    data.forEach(item => {
+        table += '<tr>';
+        Object.values(item).forEach(value => {
+            table += `<td>${value}</td>`;
+        });
+        table += '</tr>';
+    });
+    table += '</tbody></table>';
+    return table;
 }
 
 fetchData();
